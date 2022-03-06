b9f01c38604df33b87cc66ff8576874b39974976
Marco Chávez <machavezco@gmail.com>
2022-03-06 14:05:02 -0600

    Add marker and set view on clicked location, and deleting previous active  marker

--- script.js
+++ script.js
@@ -8,30 +8,32 @@ const btnSearch = document.querySelector(".btn--search");
 const btnGlobal = document.querySelector(".btn--global");
 const containerLocations = document.querySelector(".container");
 
-// Render current position
-
 class App {
   #map;
+  #zoomLevel = 15;
+  #mapEvent;
+  #activeMarker;
+
   constructor() {
     this._getPosition();
 
     btnGlobal.addEventListener("click", this._globalView.bind(this));
   }
 
-  // Get current position
+  // Get current position from geolocation API
   _getPosition = function () {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       navigator.geolocation.getCurrentPosition(
         (position) => resolve(position.coords),
         (error) => {
-          this._loadMap({ latitude: 0, longitude: 0 }, 2);
+          this._loadMap({ latitude: 0, longitude: 0 }, 2); // Default Coords
           throw new Error(
             `Could not get position, try again (${error.message})`
           );
         }
       );
     })
-      .then((coords) => this._loadMap(coords, 15))
+      .then((coords) => this._loadMap(coords, this.#zoomLevel))
       .catch((error) => {
         this._renderError(`Something went wrong!:( (${error.message})`);
       });
@@ -45,30 +47,60 @@ class App {
   _loadMap = function (location, zoomLevel) {
     const { latitude } = location;
     const { longitude } = location;
+
+    // Create map with defined bounds (top corner and bot corner)
     this.#map = L.map("map", {
       maxBounds: [
+        // Render current position
         [180, -180],
         [-180, 180],
       ],
-    }).setView([latitude, longitude], zoomLevel);
+    }).setView([latitude, longitude], zoomLevel); // Set initial view on current position
 
+    // Map style
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
     this._renderMarker([latitude, longitude]);
+    // event listener for clicks on map
+    this.#map.on("click", this._renderLocation.bind(this));
+  };
+
+  _renderLocation(mapE) {
+    this.#mapEvent = mapE;
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    // Remove active marker
+    this._removeMarker();
+
+    // Drag view to marker
+    this.#map.setView([lat, lng], this.#zoomLevel, {
+      animate: true,
+      pan: { duration: 1 },
+    });
+
+    // Render marker on clicked location
+    this._renderMarker([lat, lng]);
+
+    // Get information based on coords
+    this._getLocationData(lat, lng);
+  }
+
+  _getLocationData(lat, lng) {}
+
+  _renderMarker = function (location) {
+    this.#activeMarker = L.marker(location).addTo(this.#map);
+  };
+
+  _removeMarker = function () {
+    this.#activeMarker.remove();
   };
 
   _globalView() {
     this.#map.setView([0, 0], 2);
   }
-  _renderMarker = function (location) {
-    L.marker(location)
-      .addTo(this.#map)
-      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-      .openPopup();
-  };
 }
 
 const app = new App();
