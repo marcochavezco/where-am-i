ae294de989f1290a9db80f8dd5317944ffac913e
Marco Chávez <machavezco@gmail.com>
2022-03-09 10:26:39 -0600

    Error status text displayed on sidebar to inform the user to do certain actions based on errors
    WF

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ const inputLongitude = document.querySelector(".form__input--longitude");
 const btnCurrent = document.querySelector(".btn--current");
 const btnSearch = document.querySelector(".btn--search");
 const btnGlobal = document.querySelector(".btn--global");
+
 const containerLocations = document.querySelector(".container");
 const locations = document.querySelector(".locations");
-const errorText = document.querySelector(".error");
+const statusText = document.querySelector(".status");
+
+const mapEl = document.querySelector("#map");
 
 class App {
   #map;
@@ -33,24 +36,23 @@ class App {
           // Load map in default coords when geolocation failed
           this._loadMap({ latitude: 0, longitude: 0 }, 2);
 
-          const errorMsg = "Could not get position, try again!";
-          this._renderError(errorMsg);
-          throw new Error(
-            `Could not get position, try again (${error.message})`
-          );
+          const errorMsg = `Could not get position, try again! (${error.message})`;
+          this._renderStatus(errorMsg);
+          throw new Error(errorMsg);
         }
       );
     })
       .then(coords => this._loadMap(coords, this.#zoomLevel))
       .catch(error => {
-        this._renderError(`${error.message}`);
+        this._renderStatus(`${error.message}`);
         console.log(`Something went wrong!:( (${error.message})`);
       });
   };
 
-  _renderError = function (msg) {
-    errorText.value = "Hello";
-    errorText.classList.remove("hidden");
+  _renderStatus = function (msg) {
+    statusText.classList.remove("hidden");
+    statusText.innerHTML = msg;
+    mapEl.addEventListener("click", _ => statusText.classList.add("hidden"));
   };
 
   // Load map in current position
@@ -107,21 +109,21 @@ class App {
     // Reverse geocodin from clicked location
     this._getJSON(
       `https://geocode.xyz/${lat},${lng}?geoit=json`,
-      "Response number are limit to 1 request per second, try again"
+      "Response number are limit to 1 request per second, try again!"
     )
       .then(data => {
         if (data.matches === null)
-          throw new Error("No matches found. Try other place, please");
+          throw new Error("No matches found. Try another place!");
 
         city = data.city[0] + data.city.slice(1).toLowerCase();
         const country = data.prov; // ex. MX, USA, JP
 
-        if (!data) throw new Error(`Location not found ${data.status}`);
-        if (!data.city) throw new Error(`City not found ${data.city}`);
+        if (!data) throw new Error(`Location not found`);
+        if (!data.city) throw new Error(`City not found`);
 
         // Does not render location if recentLocation === city
         if (this.#recentLocation === city)
-          throw new Error(`Location already displayed`);
+          throw new Error("Location already displayed, try another!");
         this.#recentLocation = city;
 
         // Get country data
@@ -131,12 +133,15 @@ class App {
         );
       })
       .then(countryData => this._renderData(city, countryData, lat, lng))
-      .catch(error => console.log(error.message));
+      .catch(error => {
+        this._renderStatus(error.message);
+        console.log(error.message);
+      });
   }
 
   _getJSON(url, errorMsg) {
     return fetch(url).then(response => {
-      if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+      if (!response.ok) throw new Error(errorMsg);
       return response.json();
     });
   }
