467a517d7ded800ac287468668851749edcc0ad8
Marco Chávez <machavezco@gmail.com>
2022-03-11 15:23:20 -0600

    Form complete functionalty
    use current location button functionality
    
    WIP
    
    WIP
    
    WIP
    
    WF

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 const form = document.querySelector(".form");
 const inputLatitude = document.querySelector(".form__input--latitude");
 const inputLongitude = document.querySelector(".form__input--longitude");
+const btnForm = document.querySelector(".form__btn");
 const btnCurrent = document.querySelector(".btn--current");
 const btnSearch = document.querySelector(".btn--search");
 const btnGlobal = document.querySelector(".btn--global");
+const btnRow = document.querySelector(".btn__row");
 
 const containerLocations = document.querySelector(".container");
 const locations = document.querySelector(".locations");
@@ -16,58 +18,54 @@ const mapEl = document.querySelector("#map");
 class App {
   #map;
   #zoomLevel = 10;
-  #mapEvent;
   #activeMarker;
   #recentLocation;
 
   constructor() {
-    this._getPosition();
+    this._renderInitialPosition();
+
+    form.addEventListener("submit", this._searchLocation.bind(this));
+    btnSearch.addEventListener("click", this._searchLocation.bind(this));
+
+    btnCurrent.addEventListener(
+      "click",
+      this._renderCurrentPosition.bind(this)
+    );
 
     btnGlobal.addEventListener("click", this._globalView.bind(this));
   }
 
   // Get current position from geolocation API
-  _getPosition = function () {
-    return new Promise(resolve => {
+  _renderInitialPosition = function () {
+    new Promise(resolve => {
       navigator.geolocation.getCurrentPosition(
         position => resolve(position.coords),
 
         error => {
-          // Load map in default coords when geolocation failed
-          this._loadMap({ latitude: 0, longitude: 0 }, 2);
-
-          const errorMsg = `Could not get position, try again! (${error.message})`;
+          this._createMap(0, 0, 2); // Create map in default coords when geolocation failed
+          const errorMsg = `Could not get position (${error.message})`;
           this._renderStatus(errorMsg);
           throw new Error(errorMsg);
         }
       );
     })
-      .then(coords => this._loadMap(coords, this.#zoomLevel))
+      .then(coords => {
+        this._renderInitialMapPosition(coords, this.#zoomLevel); // Render map on current position then render position data
+      })
       .catch(error => {
         this._renderStatus(`${error.message}`);
         console.log(`Something went wrong!:( (${error.message})`);
       });
   };
 
-  _renderStatus = function (msg) {
-    statusText.classList.remove("hidden");
-    statusText.innerHTML = msg;
-    mapEl.addEventListener("click", _ => statusText.classList.add("hidden"));
-  };
-
-  // Load map in current position
-  _loadMap = function (location, zoomLevel) {
-    const { latitude } = location;
-    const { longitude } = location;
-
+  _createMap = function (lat, lng, zoomLevel) {
     // Create map with defined bounds (top corner and bot corner)
     this.#map = L.map("map", {
       maxBounds: [
-        // Render current position
         [180, -180],
         [-180, 180],
       ],
-    }).setView([latitude, longitude], zoomLevel); // Set initial view on current position
+    }).setView([lat, lng], zoomLevel); // Set initial view on current position (0,0 when geolocation failed)
 
     // Map style
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -75,53 +73,69 @@ class App {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
-    this._renderMarker([latitude, longitude]);
-
-    // render current location
-
-    // event listener for clicks on map
+    // Event listener for clicks on map, it returns a map event (mapE)
     this.#map.on("click", this._renderLocation.bind(this));
   };
 
+  // Load map and render current position
+  _renderInitialMapPosition = function (location, zoomLevel) {
+    const { latitude, longitude } = location;
+
+    this._createMap(latitude, longitude, zoomLevel);
+
+    // Render marker on initial location
+    this._renderMarker([latitude, longitude]);
+
+    // Render current location
+    const mapE = { latlng: { lat: latitude, lng: longitude } };
+    this._renderLocation(mapE);
+  };
+
   _renderLocation(mapE) {
-    this.#mapEvent = mapE;
-    const { lat, lng } = this.#mapEvent.latlng;
+    const { lat, lng } = mapE.latlng;
 
     // Remove active marker
-    this._removeMarker();
+    if (this.#activeMarker) this._removeMarker();
 
-    // Drag view to marker
+    // Drag view to new location
     this.#map.setView([lat, lng], this.#zoomLevel, {
       animate: true,
       pan: { duration: 1 },
     });
 
-    // Render marker on clicked location
+    // Render marker on clicked/searched location
     this._renderMarker([lat, lng]);
 
     // Get information based on coords
     this._getLocationData(lat, lng);
+
+    this._hideForm();
   }
 
   _getLocationData(lat, lng) {
     let city;
+    let country;
 
-    // Reverse geocodin from clicked location
+    // Reverse geocoding from clicked/searched location
     this._getJSON(
       `https://geocode.xyz/${lat},${lng}?geoit=json`,
-      "Response number are limit to 1 request per second, try again!"
+      "Response number limit to 1 request per second, try again!"
     )
       .then(data => {
         if (data.matches === null)
           throw new Error("No matches found. Try another place!");
-
-        city = data.city[0] + data.city.slice(1).toLowerCase();
-        const country = data.prov; // ex. MX, USA, JP
-
         if (!data) throw new Error(`Location not found`);
-        if (!data.city) throw new Error(`City not found`);
+        if (!data.city)
+          throw new Error(
+            `For better results and location information use accurate coordinates`
+          );
 
-        // Does not render location if recentLocation === city
+        city = `
+        ${data.city[0].toUpperCase()}${data.city.slice(1).toLowerCase()}
+          `;
+        country = data.prov; // ex. MX, USA, JP
+
+        // Does not render location if previous location === new location
         if (this.#recentLocation === city)
           throw new Error("Location already displayed, try another!");
         this.#recentLocation = city;
@@ -137,13 +151,6 @@ class App {
         this._renderStatus(error.message);
         console.log(error.message);
       });
-  }
-
-  _getJSON(url, errorMsg) {
-    return fetch(url).then(response => {
-      if (!response.ok) throw new Error(errorMsg);
-      return response.json();
-    });
   }
 
   _renderData(city, country, lat, lng) {
@@ -170,6 +177,77 @@ class App {
     locations.insertAdjacentHTML("afterbegin", html);
   }
 
+  _searchLocation = function (e) {
+    e.preventDefault();
+
+    // Allow to use search button to open form, then search location
+    if (form.classList.contains("hidden")) {
+      btnRow.style.borderRadius = "0 0 1rem 1rem";
+      btnRow.style.backgroundColor = "var(--color-dark--2)";
+      form.classList.remove("hidden");
+      return;
+    }
+
+    const lat = inputLatitude.value;
+    const lng = inputLongitude.value;
+
+    // RegEx to filter user input
+    const r = /^[ ]*?(-?\d+\.?\d*)[ ]*$/i;
+
+    if (!lat.match(r) && !lng.match(r)) {
+      const errorMsg = "Use valid coords, please";
+      this._renderStatus(errorMsg);
+      throw Error(errorMsg);
+    }
+
+    const mapE = { latlng: { lat: lat, lng: lng } };
+    this._renderLocation(mapE);
+
+    this._hideForm();
+  };
+
+  _renderCurrentPosition = function (e) {
+    e.preventDefault();
+
+    const current = new Promise(resolve => {
+      navigator.geolocation.getCurrentPosition(
+        position => resolve(position.coords),
+
+        error => {
+          this._renderStatus(
+            `Could not get position, try again! (${error.message})`
+          );
+        }
+      );
+    }).then(coords => {
+      return {
+        latlng: { lat: coords.latitude, lng: coords.longitude },
+      };
+    });
+
+    current.then(mapE => this._renderLocation(mapE));
+
+    this._hideForm();
+  };
+
+  _hideForm = function () {
+    form.classList.add("hidden");
+    btnRow.style.borderRadius = "1rem";
+    btnRow.style.backgroundColor = "var(--color-dark--1)";
+    inputLatitude.value = inputLongitude.value = "";
+  };
+
+  _renderStatus = function (msg) {
+    // Render text on sidebar
+    statusText.classList.remove("hidden");
+    statusText.innerHTML = msg;
+
+    mapEl.addEventListener("click", _ => statusText.classList.add("hidden"));
+    btnSearch.addEventListener("click", _ =>
+      statusText.classList.add("hidden")
+    );
+  };
+
   _renderMarker = function (location) {
     this.#activeMarker = L.marker(location).addTo(this.#map);
   };
@@ -180,6 +258,14 @@ class App {
 
   _globalView() {
     this.#map.setView([0, 0], 2);
+    this._hideForm();
+  }
+
+  _getJSON(url, errorMsg) {
+    return fetch(url).then(response => {
+      if (!response.ok) throw new Error(errorMsg);
+      return response.json();
+    });
   }
 }
 
